576830fb8d7e3602fd9ca7a3ff027facedc82136
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 19:39:42 +0000

    complete bonus custom validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -15,6 +15,12 @@ class Store
   validates :name, :annual_revenue, presence: true
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    errors.add(:mens_apparel, "or womens apparel must be true") unless mens_apparel || womens_apparel
+  end
+
 end
 
 class Employee 
@@ -25,7 +31,14 @@ end
 
 puts "What is your store's name?"
 store = gets.chomp
-errors = Store.create({name: store}).errors.full_messages
+
+puts "Does your store sell mens apparel? (y or n)"
+sells_mens_apparel = gets.chomp == "y" ? true : false
+
+puts "Does your store sell womens apparel (y or n)?"
+sells_womens_apparel = gets.chomp == "y" ? true : false
+
+errors = Store.create({ name: store, mens_apparel: sells_mens_apparel, womens_apparel: sells_womens_apparel }).errors.full_messages
 
 errors.each { |message|
   puts message
