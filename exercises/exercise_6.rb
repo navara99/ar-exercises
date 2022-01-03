338f18b4f1c00551ce4a932b295fc5ccbce6edf0
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 18:13:28 +0000

    add employees to store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -21,3 +21,7 @@ end
 @store1.employees.create(first_name: "Tharsikan", last_name: "Navaratnam", hourly_rate: 60)
 @store1.employees.create(first_name: "Yadiel", last_name: "Wright", hourly_rate: 45)
 @store1.employees.create(first_name: "Katrina", last_name: "Rivers", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Adonis", last_name: "Berg", hourly_rate: 20)
+@store2.employees.create(first_name: "Alondra", last_name: "Lambert", hourly_rate: 25)
+@store2.employees.create(first_name: "Eliana", last_name: "Robertson", hourly_rate: 30)
