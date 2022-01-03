eb45fd84260a1502cdb96b45e352465834cd3022
Tharsikan <thar.navaratnam@gmail.com>
2022-01-03 19:20:34 +0000

    validate columns for in stores/employees table

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Store 
+  validates :name, :annual_revenue, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0}
+end
+
+class Employee 
+  validates :first_name, :last_name, :hourly_rate, presence: true
+  validates_associated :store
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+end
+
+puts "What is your store's name?"
+store = gets.chomp
+errors = Store.create({name: store}).errors.full_messages
+
+errors.each { |message|
+  puts message
+}
+
+
+
